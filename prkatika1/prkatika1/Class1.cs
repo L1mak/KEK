d1662c496bbb400b0f8897b64a1118882f0e6ff4
L1mak <darksoop@gmail.com>
2020-02-13 22:20:16 +0500

    koks

--- prkatika1/prkatika1/Class1.cs
+++ prkatika1/prkatika1/Class1.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace prkatika1
 {
     public class Class1
     {
+        public bool Jojo(string vin)
+        {
+            Regex reg = new Regex("[0-9A-HJ-N,P,R-Z]{17}");
+            if (!reg.IsMatch(vin)) { return true; }
+            else { return false;}
+        }
 
     }
 }

3943dc74f633f78774091332bc5fb361e18aa70c
L1mak <darksoop@gmail.com>
2020-02-14 20:58:45 +0500

    Nice!

--- prkatika1/prkatika1/Class1.cs
+++ prkatika1/prkatika1/Class1.cs
@@ -9,12 +9,16 @@ namespace prkatika1
 {
     public class Class1
     {
-        public bool Jojo(string vin)
+        public bool CheckVIN(string vin)
         {
             Regex reg = new Regex("[0-9A-HJ-N,P,R-Z]{17}");
             if (!reg.IsMatch(vin) && reg.IsMatch(@"\d${4}")) { return false;}
             return true;
         }
-
+        public string GetVINCountry(string vin)
+        {
+            Regex country = new Regex("[]");
+            if 
+        }
     }
 }

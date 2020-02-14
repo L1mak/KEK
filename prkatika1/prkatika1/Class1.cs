07e07470c82e8fec99eb193eb51e500bed5eae4d
L1mak <darksoop@gmail.com>
2020-02-14 22:14:32 +0500

    Noice

--- prkatika1/prkatika1/Class1.cs
+++ prkatika1/prkatika1/Class1.cs
@@ -12,15 +12,40 @@ namespace prkatika1
         public bool CheckVIN(string vin)
         {
             Regex reg = new Regex("[0-9A-HJ-N,P,R-Z]{17}");
-            if (reg.IsMatch(@"\d${4}")){
+            if (reg.IsMatch(@"\d${4}"))
+            {
                 if (!reg.IsMatch(vin)) { return false; }
                 return true;
             }
+            return true;
+        }
         }
         public string GetVINCountry(string vin)
         {
-            Regex country = new Regex("[]");
-            if 
+        Dictionary<string,string> countrydic = new Dictionary<string,string>();
+            Regex country = new Regex("[A-HJ-NPR-Z0-9]{8}[1-9X][A-HN-PR-TV-Y1-9][A-HJ-NPR-Z0-9]{3}[0-9]{4}");
+        if (countrydic == null){
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+            countrydic.Add();
+        }
+        return(countryname);
         }
     }
-}
+

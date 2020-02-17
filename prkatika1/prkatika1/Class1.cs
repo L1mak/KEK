169b8d40eb58a2354d9dc0251cad080e962dc4f8
L1mak <darksoop@gmail.com>
2020-02-17 21:17:07 +0500

    LOL

--- prkatika1/prkatika1/Class1.cs
+++ prkatika1/prkatika1/Class1.cs
@@ -15,7 +15,7 @@ namespace prkatika1
             if (!reg.IsMatch(@"\d${4}"))
             {
                 if (!reg.IsMatch(vin)) { return false; }
-                return false;
+                return false; 
             }
             return true;
         }
@@ -25,25 +25,57 @@ namespace prkatika1
         Dictionary<string,string> countrydic = new Dictionary<string,string>();
             Regex country = new Regex("[A-HJ-NPR-Z0-9]{8}[1-9X][A-HN-PR-TV-Y1-9][A-HJ-NPR-Z0-9]{3}[0-9]{4}");
         if (countrydic == null){
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
-            countrydic.Add();
+            countrydic.Add("AA-AH","");
+            countrydic.Add("AJ-AN","");
+            countrydic.Add("AP-A0","");
+            countrydic.Add("BA-BE", "");
+            countrydic.Add("BF-BK", "");
+            countrydic.Add("BL-BR", "");
+            countrydic.Add("BS-B0", "");
+            countrydic.Add("CA-CE", "");
+            countrydic.Add("CF-CK", "");
+            countrydic.Add("CL-CR", "");
+            countrydic.Add("CS-C0", "");
+            countrydic.Add("DA-DE", "");
+            countrydic.Add("DF-DK", "");
+            countrydic.Add("DL-DR", "");
+            countrydic.Add("JA-JT", "");
+            countrydic.Add("EA-EE", "");
+            countrydic.Add("EF-EK", "");
+            countrydic.Add("KA-KE", "");
+            countrydic.Add("FA-FE", "");
+            countrydic.Add("FF-FK", "");
+            countrydic.Add("KF-KK", "");
+            countrydic.Add("KL-KR", "");
+            countrydic.Add("KS-K0", "");
+            countrydic.Add("LA-L0", "");
+            countrydic.Add("MA-ME", "");
+            countrydic.Add("MF-MK", "");
+            countrydic.Add("ML-MR", "");
+            countrydic.Add("PA-PE", "");
+            countrydic.Add("NF-NK", "");
+            countrydic.Add("NL-NR", "");
+            countrydic.Add("PF-PK", "");
+            countrydic.Add("PL-PR", "");
+            countrydic.Add("RA-RE", "");
+            countrydic.Add("RF-RK", "");
+            countrydic.Add("RL-RR", "");
+            countrydic.Add("RS-R0", "");
+            countrydic.Add("SA-SM", "");
+            countrydic.Add("SN-ST", "");
+            countrydic.Add("SU-SZ", "");
+            countrydic.Add("S1-S4", "");
+            countrydic.Add("TA-TH", "");
+            countrydic.Add("TJ-TP", "");
+            countrydic.Add("TR-TV", "");
+            countrydic.Add("TW-T1", "");
+            countrydic.Add("UH-UM", "");
+            countrydic.Add("UN-UT", "");
+            countrydic.Add("UU-UZ", "");
+            countrydic.Add("", "");
+            countrydic.Add("", "");
+            countrydic.Add("", "");
+            countrydic.Add("", "");
         }
         return(countryname);
         }

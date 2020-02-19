c8353a01ac6cc65456f797a796fcc715378ee77b
L1mak <darksoop@gmail.com>
2020-02-19 21:38:14 +0500

    lopo

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection("Data source = 303-9\\MSSQLSERVERRR;Inital Catalog = BAZA; Integrated Security = true;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect.Open();
+
+        }
+    }
+}

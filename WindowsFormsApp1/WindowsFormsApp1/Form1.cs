da49ec7fca946ccdea19446399d0441e46062321
L1mak <darksoop@gmail.com>
2020-02-19 21:59:47 +0500

    koko

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        SqlConnection connect = new SqlConnection("Data source = 303-9\\MSSQLSERVERRR;Inital Catalog = BAZA; Integrated Security = true;");
+        SqlConnection connect = new SqlConnection("Data source = 303-9\\MSSQLSERVERRR; Initial Catalog = BAZA; Integrated Security = true;");
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +22,22 @@ namespace WindowsFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             connect.Open();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand auth = new SqlCommand($"SELECT * FROM BAZA WHERE login  = '{textBox1.Text}' AND password = '{textBox2.Text}'", connect);
+            if (auth) { }
 
         }
     }
